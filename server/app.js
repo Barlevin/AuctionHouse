972c0065543a3a25b0cf623bf8051545eb027951
Bar Levin <barlevinn@gmail.com>
2025-10-28 14:08:45 +0200

    dsadsadsadas

--- server/app.js
+++ server/app.js
@@ -62,11 +62,10 @@ function cleanupOldItems() {
 cron.schedule('0 7 * * *', cleanupOldItems);
 
 // Serve static files from React build folder
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, "../client/build")));
 
-// Handle React routing - return all requests to React app
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
 });
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
